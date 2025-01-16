8f6562730dcb3571e08f33b2ad5d98f901b7a5ad
ha684 <robotpro682004@gmail.com>
2025-01-16 18:09:46 +0700

    Update model_load.py

--- model_load.py
+++ model_load.py
@@ -2,12 +2,12 @@ import os
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-from diffusers import StableDiffusion3Pipeline
+from diffusers import StableDiffusion3Pipeline,BitsAndBytesConfig, SD3Transformer2DModel
 
 def load_llama_model(model_id="meta-llama/Llama-3.2-3B-Instruct"):
     model = AutoModelForCausalLM.from_pretrained(
         model_id, 
-        device_map="auto", 
+        device_map="cuda:1", 
         torch_dtype=torch.float16, 
         trust_remote_code=True, 
     )
@@ -21,9 +21,22 @@ def load_llama_model(model_id="meta-llama/Llama-3.2-3B-Instruct"):
     return pipe
 
 def load_stablediffusion_model(model_id="stabilityai/stable-diffusion-3.5-medium"):
-    pipe = StableDiffusion3Pipeline.from_pretrained(
-        model_id, 
-        torch_dtype=torch.float16,
-        device_map="balanced"
+    nf4_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16
     )
-    return pipe
+    model_nf4 = SD3Transformer2DModel.from_pretrained(
+        model_id,
+        subfolder="transformer",
+        quantization_config=nf4_config,
+        torch_dtype=torch.float16
+    )
+
+    pipeline = StableDiffusion3Pipeline.from_pretrained(
+        model_id, 
+        transformer=model_nf4,
+        torch_dtype=torch.float16
+    )
+    pipeline.enable_model_cpu_offload()
+    return pipeline
